fbaf9d50e19f82607835075843c2ea8f9f58fffb
wallaceleonel <wallace.leonell@outlook.com>
2024-11-22 13:52:06 -0300

    first Commit

---
+++ src/Api.Render/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Render.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "getTeste")]
+        public string Get() => "Está tudo funcionando corretamente";
+    }
+}
